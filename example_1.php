143f7b0c8ebe52151f8722775d7876a94e068dc9
ilya <microstim@mail.ru>
2020-06-03 15:54:16 +0300

    example1

---
+++ example_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>hello world php</title>
+</head>
+<body>
+  
+  <?php   
+  // Изучение PHP для начинающих | Урок #3 - Пишем "Hello World!"
+    echo "hello world"; // выведет на экран словосочетание "hello world"
+    echo "<br />"; // перенос на новую строку
+  ?>
+
+</body>
+</html>
